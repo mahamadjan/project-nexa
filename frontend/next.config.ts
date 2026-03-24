cb88583b61bf8e362f2fb7ac60bbec8593b0fff9
Akhmed <yakhubovv0708@gmail.com>
2026-03-25 04:54:37 +0600

    Очистка от мусора и сохранение сайта

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['three', '@react-three/fiber', '@react-three/drei', 'framer-motion', 'lucide-react'],
-  turbopack: {
-    root: __dirname,
-  },
 };
 
 export default nextConfig;
